b8b4049c39e846d91428f3f315a7f06d8c645340
Abdullah Abu Soboh <abdullah.abusoboh@gmail.com>
2025-08-05 03:19:20 +0300

    Solve the previous problem + delete redundant files like __manifest__.py and ar_po

--- views/__manifest__.py
+++
@@ -1,8 +0,0 @@
-#'data': [
- #   'views/book_views.xml',
-  #  'views/author_views.xml',
-   # 'views/membership_request_view.xml',
-    #'views/res_partner_view.xml',
-    #'views/product_category_view.xml',
-#],
-
